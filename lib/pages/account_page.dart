d87f9d0d000e27fa61ad25555b6ab19b44fc1ae2
sirdeelord <saddyodus@yahoo.com>
2020-02-01 00:09:48 -0500

    ui pages

---
+++ lib/pages/account_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: Text('Account Page', style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 28
+            ))
+        );
+    }
+}
