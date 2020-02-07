5025bd4d957ae8649be958abdfa7001f1696aec9
sirdeelord <saddyodus@yahoo.com>
2020-02-07 00:19:33 -0500

    new pages

---
+++ lib/pages/notifications_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../bloc.navigation_bloc/navigation_bloc.dart';
+
+class NotificationsPage extends StatelessWidget with NavigationStates {
+    @override
+    Widget build(BuildContext context) {
+        return Center(
+            child: Text('Notifications Page',
+                style: TextStyle(
+                    fontFamily: 'Spacemono',
+                    fontWeight: FontWeight.w900,
+                    fontSize: 28
+                )
+            )
+        );
+    }
+}
